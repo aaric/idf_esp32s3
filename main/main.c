417c5cd3004c8a6c296748e80251e1f9a6122583
qlhan <qlhan@incarcloud.com>
2022-02-16 09:57:10 +0800

    0.2.0 log

--- main/main.c
+++ main/main.c
@@ -1,17 +1,12 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "esp_log.h"
 
+
+static const char* TAG = "ch02_log"
 
 void app_main(void)
 {
@@ -20,21 +15,34 @@ void app_main(void)
     /* Print chip information */
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+    ESP_LOGI(TAG, "This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
             chip_info.cores,
             (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
             (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
 
-    printf("silicon revision %d, ", chip_info.revision);
+    ESP_LOGI(TAG, "silicon revision %d, ", chip_info.revision);
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+    ESP_LOGI(TAG, "%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    /* ESP_LOGx testing */
+    while(1) {
+        // error level
+        ESP_LOGE(TAG, "error log");
+
+        // warn level
+        ESP_LOGW(TAG, "warn log");
+
+        // info level
+        ESP_LOGI(TAG, "info log");
+
+        // error level
+        ESP_LOGD(TAG, "debug log");
+
+        // verbose
+        ESP_LOGV(TAG, "verbose log");
+
+        // delay 10s
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
