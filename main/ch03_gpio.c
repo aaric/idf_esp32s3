e106a3c0808995c07bbb644b1b893743c19dd014
qlhan <qlhan@incarcloud.com>
2022-03-22 13:41:09 +0800

    0.3.0 gpio intr

--- main/ch03_gpio.c
+++ main/ch03_gpio.c
@@ -10,10 +10,34 @@
 #define LED_RGB_GPIO GPIO_NUM_48
 #define LED_RMT_CHANNEL RMT_CHANNEL_0
 
+#define BUTTON_BOOT_GPIO GPIO_NUM_0
+#define GPIO_INPUT_PIN_SEL 1ULL << BUTTON_BOOT_GPIO
+#define ESP_INTR_FLAG_DEFAULT 0
+
 static const char *TAG = "ch03_gpio";
 
 static uint8_t led_state = 0;
 static led_strip_t *p_strip_led;
+
+static xQueueHandle gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void *arg)
+{
+    uint32_t gpio_num = (uint32_t)arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void *arg)
+{
+    uint32_t io_num;
+    for (;;)
+    {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY))
+        {
+            ESP_LOGE(TAG, "GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
 
 static void led_rmt_init()
 {
@@ -59,6 +83,22 @@ void app_main(void)
     // gpio_set_direction(LED_RGB_GPIO, GPIO_MODE_OUTPUT);
 
     led_rmt_init();
+
+    gpio_config_t gpio_conf;
+    gpio_conf.intr_type = GPIO_INTR_NEGEDGE;
+    gpio_conf.mode = GPIO_MODE_INPUT;
+    gpio_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    gpio_conf.pull_up_en = 1;
+
+    gpio_config(&gpio_conf);
+    gpio_set_intr_type(BUTTON_BOOT_GPIO, GPIO_INTR_POSEDGE);
+
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    gpio_isr_handler_add(BUTTON_BOOT_GPIO, gpio_isr_handler, (void *)BUTTON_BOOT_GPIO);
 
     while (1)
     {
